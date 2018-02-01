a30f8044304dccf42ccbe9bdfa52ff619bb85a8b
Enrique Garcia <rickyg218@gmail.com>
2018-02-01 15:03:34 -0600

    Frieda Daily MEx

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,24 +1,49 @@
 var hangman;
 
-// function Hangman() {
 
-// }
+function Hangman() {
+  this.words=['y','k','w'];
+  this.secretWord = "";
+  this.letters = ["a","b","c"];
+  this.guessedLetter= [];
+  this.errorsLeft = 10; 
 
-// Hangman.prototype.getWord = function () {
 
-// };
+Hangman.prototype.getWord = function () {
+ return 'this.words';
+  }
+}
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
 
-// };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+if( this.letters > 2){
+  return true;
+}
+else{
+  return false;
+}
 
-// };
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkClickedLetters = function (key) {
+  var keys="p"
+  for (var i =0; i=>this.words.length;i++){
+    if(this.letters[i] === key){
+      return "letters already choosen";
+    }else{
+      return false;
+    }
+    
+  }
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+var key ='N';
+hangman.checkClickedLetters(key);
+return "key"
+key.push('Ironhack')
+};
 
 // Hangman.prototype.addWrongLetter = function (letter) {
 
